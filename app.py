f1450bd1aaf7054a6f8e012d2d24cef8b6e42a5a
Sourish Vishwanatham <sourishvishwanatham@gmail.com>
2023-12-06 21:26:45 +0000

    v0.2 note-test on home network

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/player_stats')
-def player_stats():
-    player = request.args.get('my_player')
+@app.route('/nba')
+def get_stats():
+    my_player = request.args.get('my_player')
 
     # Check for empty strings or string with only spaces
-    if not bool(player.strip()):
+    if not bool(my_player.strip()):
         # You could render "City Not Found" instead like we do below
         my_player = "Tyler Herro"
 
     player_stats = get_player_stats(my_player)
 
     # City is not found by API
-    if not player_stats['cod'] == 200:
-        return render_template('city-not-found.html')
+    #if not player_stats == 200:
+      #  return render_template('player-not-found.html')
 
     return render_template(
         "nba.html",
@@ -36,5 +36,5 @@ def player_stats():
 
 
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+serve(app, host="0.0.0.0", port=8000)
--- nba.py
+++ nba.py
@@ -11,27 +11,22 @@ load_dotenv()
 def get_player_stats(my_player="Tyler Herro"):
 
     nba_players = players.get_players()
-
-    player_id = [player[0]["id"] for player in nba_players if player["full_name"] == my_player]
-
-    player_stats = playercareerstats.PlayerCareerStats(player_id)
-
+    print("test1")
+    my_id = [player["id"] for player in nba_players if player["full_name"] == my_player]
+    print(my_id)
+    player_stats = playercareerstats.PlayerCareerStats(my_id)
+    print("test3")
     stat_frame = player_stats.get_data_frames()[0]
-
-    return stat_frame.iloc[-1]
+    current_stat_idx = len(stat_frame)-1
+    return stat_frame.iloc[current_stat_idx]
 
 
 if __name__ == "__main__":
     print('\n*** Get NBA Stats ***\n')
 
-    city = input("\nPlease enter a player name: ")
+    my_player = input("\nPlease enter a player name: ")
 
-    # Check for empty strings or string with only spaces
-    # This step is not required here
-    # if not bool(city.strip()):
-    #     city = "Kansas City"
-
-    player_stats = get_player_stats(city)
+    player_stats = get_player_stats(my_player)
 
     print("\n")
     pprint(player_stats)
