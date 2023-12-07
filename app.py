278e4fff0145198546c52c3169c7c21da3ad7ae4
sourishv <sourishvishwanatham@gmail.com>
2023-12-07 11:45:28 -0500

    v0.3 working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from nba import get_player_stats
 from waitress import serve
+import numpy as np
 
 app = Flask(__name__)
 
@@ -15,26 +16,21 @@ def index():
 def get_stats():
     my_player = request.args.get('my_player')
 
-    # Check for empty strings or string with only spaces
     if not bool(my_player.strip()):
-        # You could render "City Not Found" instead like we do below
         my_player = "Tyler Herro"
 
-    player_stats = get_player_stats(my_player)
-
-    # City is not found by API
-    #if not player_stats == 200:
-      #  return render_template('player-not-found.html')
+    nba_fcn_output = get_player_stats(my_player)
+    player_stats = nba_fcn_output[0]
 
     return render_template(
         "nba.html",
-        player = player_stats["full_name"],
-        PTS = player_stats["PTS"],
-        REB = player_stats["REB"],
-        AST = player_stats["AST"]
+        player = nba_fcn_output[1],
+        ppg = np.round(player_stats["PTS"]/player_stats["GP"], 1),
+        rpg = np.round(player_stats["REB"]/player_stats["GP"], 1),
+        apg = np.round(player_stats["AST"]/player_stats["GP"], 1)
     )
 
 
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+serve(app, host="0.0.0.0", port=8000)
--- nba.py
+++ nba.py
@@ -2,22 +2,32 @@ from pprint import pprint
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 import pandas as pd
+from difflib import get_close_matches
 
 def get_player_stats(my_player='Tyler Herro'):
 
     nba_players = players.get_players()
     print("test1")
+
+    player_names = [player['full_name'] for player in nba_players]
+    my_player = get_close_matches(my_player, player_names, 1, 0)[0]
+    print(my_player)
+
     player_dict = [
         player for player in nba_players if player['full_name'] == my_player
         ][0]
-    print(player_dict)
+
     my_id = str(player_dict["id"])
     print(type(my_id))
+
     my_player_stats = playercareerstats.PlayerCareerStats(player_id=my_id)
+
     stat_frame = my_player_stats.get_data_frames()[0]
     print(stat_frame)
+
     current_stat_idx = len(stat_frame)-1
-    return stat_frame.iloc[current_stat_idx]
+
+    return (stat_frame.iloc[current_stat_idx], my_player)
 
 
 if __name__ == "__main__":
