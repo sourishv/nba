2e7fb1145aff6bf27a028963f6825e17fd3d71c2
Sourish Vishwanatham <sourishvishwanatham@gmail.com>
2023-12-07 15:04:33 +0000

    v0.2

--- nba.py
+++ nba.py
@@ -12,9 +12,13 @@ def get_player_stats(my_player="Tyler Herro"):
 
     nba_players = players.get_players()
     print("test1")
-    my_id = [player["id"] for player in nba_players if player["full_name"] == my_player]
-    print(my_id)
-    player_stats = playercareerstats.PlayerCareerStats(my_id)
+    my_player_data = [
+        player for player in nba_players if player["full_name"] == my_player
+        ][0]
+    print(my_player_data)
+    my_id = my_player_data["id"]
+    print(type(my_id))
+    player_stats = playercareerstats.PlayerCareerStats(player_id=str(my_id))
     print("test3")
     stat_frame = player_stats.get_data_frames()[0]
     current_stat_idx = len(stat_frame)-1
