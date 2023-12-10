680ea457bcfd3330f1e8815f78459834a4e45979
sourishv <sourishvishwanatham@gmail.com>
2023-12-10 14:07:37 -0500

    updated formatting

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def autocomplete():
 
     return jsonify(suggestions)
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+serve(app, host="0.0.0.0", port=8000)
