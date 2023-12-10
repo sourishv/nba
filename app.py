48d8700b336fd0aea228a9da30816914fb4f4b12
sourishv <sourishvishwanatham@gmail.com>
2023-12-10 10:45:17 -0500

    more cleanup in nba & app

--- app.py
+++ app.py
@@ -18,16 +18,11 @@ def get_stats():
         player2 = request.form.get('player2')
         selected_stats = request.form.getlist('stats[]')
 
-        if not bool(player1.strip()):
-            player1 = "Trae Young"
-        if not bool(player2.strip()):
-            player2 = "Tyler Herro"
-
         player1_name, player1_id, stats1 = get_player_stats(player1, selected_stats)[:3]
         player2_name, player2_id, stats2 = get_player_stats(player2, selected_stats)[:3]
 
-        rounded_stats1 = {key: round(value, 2) if isinstance(value, (int, float)) else value for key, value in stats1.items()}
-        rounded_stats2 = {key: round(value, 2) if isinstance(value, (int, float)) else value for key, value in stats2.items()}
+        rounded_stats1 = {key: round(value, 2) for key, value in stats1.items()}
+        rounded_stats2 = {key: round(value, 2) for key, value in stats2.items()}
 
         player1_image_url = get_player_image_url(player1_id)
         player2_image_url = get_player_image_url(player2_id)
@@ -41,13 +36,12 @@ def get_stats():
             better_player = player2_name
         else:
             better_player = "Neither player"
-        print(rounded_stats1)
         return render_template('nba.html', title='Compare NBA Players:', player1=player1_name, player2=player2_name,
                                selected_stats=selected_stats, stats1=rounded_stats1, stats2=rounded_stats2,
                                better_player=better_player, player1_image_url=player1_image_url,
                                player2_image_url=player2_image_url)
     
-    return render_template('index.html')  # Replace with your actual index template name
+    return render_template('index.html')
 
 @app.route('/new_comparison')
 def new_comparison():
--- nba.py
+++ nba.py
@@ -6,16 +6,13 @@ import pandas as pd
 def get_player_image_url(player_id):
     return f"https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{player_id}.png"
 
-def get_player_stats(my_player='Tyler Herro', selected_stats=None):
+def get_player_stats(my_player="LeBron James", selected_stats=None):
     nba_players = players.get_players()
 
     player_names = [player['full_name'] for player in nba_players]
     my_player = get_close_matches(my_player, player_names, 1, 0)[0]
 
     player_dict = next((player for player in nba_players if player['full_name'] == my_player), None)
-
-    if player_dict is None:
-        return {"error": "Player not found."}
 
     my_id = str(player_dict["id"])
     my_player_stats = playercareerstats.PlayerCareerStats(player_id=my_id)
@@ -52,21 +49,3 @@ def get_player_stats(my_player='Tyler Herro', selected_stats=None):
         stats_dict = combined_data.to_dict()
 
     return my_player, my_id, stats_dict, player_names
-if __name__ == "__main__":
-    print('\n*** Get NBA Stats ***\n')
-
-    my_player = input("\nPlease enter a player name: ")
-    selected_stats = input("\nPlease enter selected stats (comma-separated): ").split(',')
-
-    player_data, player_id = get_player_stats(my_player, selected_stats)
-
-    if "error" in player_data:
-        print(player_data["error"])
-    else:
-        player_stats = player_data["stats"]
-        player_name = player_data["player_name"]
-        player_image_url = get_player_image_url(player_id)
-
-        print(f"\nStats for {player_name}:\n")
-        print(player_stats)
-        print(f"Player Image URL: {player_image_url}")
